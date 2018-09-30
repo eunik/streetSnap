c0615583b8ab28e2ddeb25a5b27fad51265f39ed
Michelle Li <30761213+michelleli723@users.noreply.github.com>
2018-09-30 07:06:23 -0400

    Update post.py

--- post/post.py
+++ post/post.py
@@ -15,3 +15,19 @@ get_locs()
 #print(create_user('dax3ddsdddd', 'a', 'a', 'a'))
 #print(get_user_info('1','username'))
 #remove_user(7)
+
+def create_post(user_id, lat, lon, body):
+	p = Post(user_id, lat, lon, body)
+
+	db_session.add(p)
+	# make sure we update our database is updated with the id
+	db_session.flush()
+	db_session.commit()
+	return {'success': 1, 'id': p.id, 'msg': 'success'}
+
+def displayPosts():
+    for post in db_session.query(Post.id):
+        print(Post.id)
+
+print(create_post(788, 107.87, 376.9789785, ' the body'))
+displayPosts()
